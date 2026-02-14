1bc3800a3391251a0cb91d60a6c686c2f9d77de7
serankobaner <kobanerseran@gmail.com>
2026-02-13 19:15:56 -0500

    fix(terraform): relax azurerm provider version constraint

--- infra/terraform/providers.tf
+++ infra/terraform/providers.tf
@@ -6,7 +6,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.120"
+      version = ">= 3.0, < 4.0"
     }
     random = {
       source  = "hashicorp/random"
